b0fb10accf1a905db95e7fdfb9e1253a282b4a92
Mohammed Bhadsorawala <mhdbhadsorawala@gmail.com>
2024-08-30 01:48:19 +0530

    added small donate and request resource

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,63 +1,63 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
-import { FiBook, FiUsers, FiClipboard, FiTool } from "react-icons/fi";
-import { FaCalendar } from "react-icons/fa";
-import { useAuth } from "../AuthContext";
-import { useTheme } from "../ThemeContext";
-import carousel1 from "../assets/img/ngo_1.jpg";
-import carousel2 from "../assets/img/ngo_2.jpg";
-import carousel3 from "../assets/img/ngo_3.jpg";
-import tata from "../assets/img/tata.jpeg";
-import jpmc from "../assets/img/jpmc.jpg";
-import hdfc from "../assets/img/hdfc_bank.jpg";
-import reliance from "../assets/img/reliance.jpeg";
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
+import { FiBook, FiUsers, FiClipboard, FiTool } from 'react-icons/fi'
+import { FaCalendar } from 'react-icons/fa'
+import { useAuth } from '../AuthContext'
+import { useTheme } from '../ThemeContext'
+import carousel1 from '../assets/img/ngo_1.jpg'
+import carousel2 from '../assets/img/ngo_2.jpg'
+import carousel3 from '../assets/img/ngo_3.jpg'
+import tata from '../assets/img/tata.jpeg'
+import jpmc from '../assets/img/jpmc.jpg'
+import hdfc from '../assets/img/hdfc_bank.jpg'
+import reliance from '../assets/img/reliance.jpeg'
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
-import "./home.css";
+import './home.css'
 
 const Home = () => {
-  const { theme } = useTheme();
-  const { isLoggedIn } = useAuth();
-  const [events, setEvents] = useState([]);
-  const location = useLocation();
-  const navigate = useNavigate();
+  const { theme } = useTheme()
+  const { isLoggedIn } = useAuth()
+  const [events, setEvents] = useState([])
+  const location = useLocation()
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (isLoggedIn) {
-      const user_name = localStorage.getItem("user_name");
-      if (location.state && location.state.action === "homelogin") {
-        toast.success(`Welcome ${user_name}`);
+      const user_name = localStorage.getItem('user_name')
+      if (location.state && location.state.action === 'homelogin') {
+        toast.success(`Welcome ${user_name}`)
       }
     }
-    if (location.state && location.state.action === "homelogout") {
-      toast.info("Logout Success");
+    if (location.state && location.state.action === 'homelogout') {
+      toast.info('Logout Success')
     }
-  }, [location.state, isLoggedIn]);
+  }, [location.state, isLoggedIn])
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/auth/up_events")
+      .get('http://localhost:3000/auth/up_events')
       .then((res) => {
-        setEvents(res.data);
+        setEvents(res.data)
       })
-      .catch((err) => console.log(err));
-  }, []);
+      .catch((err) => console.log(err))
+  }, [])
 
   const formatDate = (timestamp) => {
     const options = {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-    };
-    return new Date(timestamp).toLocaleDateString("en-US", options);
-  };
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    }
+    return new Date(timestamp).toLocaleDateString('en-US', options)
+  }
 
   const sliderSettings = {
     dots: true,
@@ -68,7 +68,7 @@ const Home = () => {
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: true,
-  };
+  }
 
   return (
     <div>
@@ -112,11 +112,11 @@ const Home = () => {
                 <p
                   className="card-text"
                   style={{
-                    fontSize: "1.4rem",
-                    fontWeight: "bold",
-                    color: "#333",
-                    marginTop: "0px",
-                    display: "block",
+                    fontSize: '1.4rem',
+                    fontWeight: 'bold',
+                    color: '#333',
+                    marginTop: '0px',
+                    display: 'block',
                   }}
                 >
                   ₹63 Lakhs
@@ -132,11 +132,11 @@ const Home = () => {
                 <p
                   className="card-text"
                   style={{
-                    fontSize: "1.4rem",
-                    fontWeight: "bold",
-                    color: "#333",
-                    marginTop: "0px",
-                    display: "block",
+                    fontSize: '1.4rem',
+                    fontWeight: 'bold',
+                    color: '#333',
+                    marginTop: '0px',
+                    display: 'block',
                   }}
                 >
                   ₹51 Lakhs
@@ -152,11 +152,11 @@ const Home = () => {
                 <p
                   className="card-text"
                   style={{
-                    fontSize: "1.4rem",
-                    fontWeight: "bold",
-                    color: "#333",
-                    marginTop: "0px",
-                    display: "block",
+                    fontSize: '1.4rem',
+                    fontWeight: 'bold',
+                    color: '#333',
+                    marginTop: '0px',
+                    display: 'block',
                   }}
                 >
                   ₹42 Lakhs
@@ -176,11 +176,11 @@ const Home = () => {
                 <p
                   className="card-text"
                   style={{
-                    fontSize: "1.4rem",
-                    fontWeight: "bold",
-                    color: "#333",
-                    marginTop: "0px",
-                    display: "block",
+                    fontSize: '1.4rem',
+                    fontWeight: 'bold',
+                    color: '#333',
+                    marginTop: '0px',
+                    display: 'block',
                   }}
                 >
                   ₹30 Lakhs
@@ -192,46 +192,109 @@ const Home = () => {
       </section>
 
       <section className={`py-4 bg-${theme}`} id="upcoming-events">
-            <div className="container">
-                <h2 className="section-heading text-center">Upcoming Drives</h2>
-                <hr className="divider my-4" />
+        <div className="container">
+          <h2 className="section-heading text-center">Upcoming Drives</h2>
+          <hr className="divider my-4" />
 
-                {events.length > 0 ? (
-                    events.map((e, index) => (
-                        <div className="card mb-3 mx-auto" style={{ maxWidth: "720px" }} key={index}>
-                            <div className="row g-0">
-                                <div className="col-md-4">
-                                    <img
-                                        src="https://th.bing.com/th/id/R.644b5c8d9ba326376fb49138018483b9?rik=NUZlrAmatX8Rkw&riu=http%3a%2f%2fswitchindia.org%2fimages%2fwork%2ffree-health-check-up-camp-at-airoli04.jpg&ehk=492SNHx4zpUYB5teNFH2SI6NRebl1dhmnLQ8ev%2bLp6s%3d&risl=&pid=ImgRaw&r=0"
-                                        className="img-fluid rounded-start"
-                                        alt={e.title}
-                                    />
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="card-body">
-                                        <h5 className="card-title">{e.title}</h5>
-                                        <p className="card-text truncate" dangerouslySetInnerHTML={{ __html: e.content }}></p>
-                                        <div><small><p><b><FaCalendar className='me-1 ' />{formatDate(e.schedule)}</b></p></small></div>
-                                        <button
-                                            className="btn btn-primary float-right read_more"
-                                            onClick={() => navigate("events/view", { state: { action: "view", data: e } })}
-                                        >
-                                            Read More
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <div className="d-flex flex-column justify-content-center align-items-center">
-                        <h4 className='text-info-emphasis'>No Upcoming Drives Available</h4>
+          {events.length > 0 ? (
+            events.map((e, index) => (
+              <div
+                className="card mb-3 mx-auto"
+                style={{ maxWidth: '720px' }}
+                key={index}
+              >
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    <img
+                      src="https://th.bing.com/th/id/R.644b5c8d9ba326376fb49138018483b9?rik=NUZlrAmatX8Rkw&riu=http%3a%2f%2fswitchindia.org%2fimages%2fwork%2ffree-health-check-up-camp-at-airoli04.jpg&ehk=492SNHx4zpUYB5teNFH2SI6NRebl1dhmnLQ8ev%2bLp6s%3d&risl=&pid=ImgRaw&r=0"
+                      className="img-fluid rounded-start"
+                      alt={e.title}
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">{e.title}</h5>
+                      <p
+                        className="card-text truncate"
+                        dangerouslySetInnerHTML={{ __html: e.content }}
+                      ></p>
+                      <div>
+                        <small>
+                          <p>
+                            <b>
+                              <FaCalendar className="me-1 " />
+                              {formatDate(e.schedule)}
+                            </b>
+                          </p>
+                        </small>
+                      </div>
+                      <button
+                        className="btn btn-primary float-right read_more"
+                        onClick={() =>
+                          navigate('events/view', {
+                            state: { action: 'view', data: e },
+                          })
+                        }
+                      >
+                        Read More
+                      </button>
                     </div>
-                )}
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <h4 className="text-info-emphasis">
+                No Upcoming Drives Available
+              </h4>
             </div>
-        </section>
+          )}
+        </div>
+      </section>
 
-      
+      <section className={`py-4 bg-${theme}`} id="donation-links">
+        <div className="container text-center">
+          <h2 className="section-heading">Get Involved</h2>
+          <hr className="divider my-4" />
+
+          <div className="row justify-content-center">
+            {/* Small Donations Link */}
+            <div className="col-md-6 mb-4">
+              <div className="card h-100 border-primary">
+                <div className="card-body">
+                  <h5 className="card-title text-primary">
+                    Support with a Small Donation
+                  </h5>
+                  <p className="card-text">
+                    Every little bit helps! Your small contribution can make a
+                    big difference in someone's life.
+                  </p>
+                  <a href="/smalldonate" className="btn btn-primary btn-block">
+                    Donate Now
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* Organization Resource Request Link */}
+            <div className="col-md-6 mb-4">
+              <div className="card h-100 border-primary">
+                <div className="card-body">
+                  <h5 className="card-title text-primary">Request Resources</h5>
+                  <p className="card-text">
+                    Are you an orphanage, old age home, or similar organization?
+                    Request resources from us to help those in need.
+                  </p>
+                  <a href="/orgform" className="btn btn-primary btn-block">
+                    Request Resources
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* <section className={`py-4 bg-${theme}`} id="upcoming-events">
         <div className="container">
@@ -301,7 +364,7 @@ const Home = () => {
         </div>
       </section>  */}
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
